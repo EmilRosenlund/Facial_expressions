7a2529c5cd939580e1b856353dbd3c324c7b8e41
EmilRosenlund <116708131+EmilRosenlund@users.noreply.github.com>
2026-03-20 12:04:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=DCASE_Test
+#SBATCH --job-name=Face_Test
 #SBATCH --mem=24G
 #SBATCH --cpus-per-task=15
 #SBATCH --gres=gpu:1
 #SBATCH --time=01:00:00
+#SBATCH --output=out.txt
+#SBATCH --error=error.txt
 
 IMAGE="/ceph/container/pytorch/pytorch_25.08.sif"
 VENV_BIND="/ceph/project/P8_DCASE/p8_env:/scratch/p8_env"
